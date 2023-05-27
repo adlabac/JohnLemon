40db6886bcfaf83467c76b53cbbcb8a64f2c5000
Aleksandar Dlabač <aleksandar@dlabac.com>
2023-05-27 13:39:21 +0200

    Kretanje neprijatelja

---
+++ Assets/Scripts/WaypointControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointControl : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    NavMeshAgent navMeshAgent;
+    int currentWaypoint = 0;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(waypoints[currentWaypoint].position);
+    }
+
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[currentWaypoint].position);
+        }
+    }
+}
