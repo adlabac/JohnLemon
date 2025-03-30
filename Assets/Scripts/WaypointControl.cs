293f5ef0d7a4481a7b1756259d6126ec41cb27f6
Aleksandar Dlabač <aleksandar@dlabac.com>
2025-03-30 11:07:04 +0200

    Dodati komentari skripti WaypointControl

--- Assets/Scripts/WaypointControl.cs
+++ Assets/Scripts/WaypointControl.cs
@@ -1,25 +1,25 @@
 using UnityEngine;
-using UnityEngine.AI; 
+using UnityEngine.AI;
 
 public class WaypointControl : MonoBehaviour
 {
-    public Transform[] waypoints;
+    public Transform[] waypoints;  // Niz tačaka (waypointa) koje neprijatelj treba da prati
 
-    NavMeshAgent navMeshAgent;
-    int currentWaypoint = 0;
+    NavMeshAgent navMeshAgent;  // Referenca ka NavMeshAgent komponenti
+    int currentWaypoint = 0;  // Tačka prema kojoj se neprijatelj/nav mesh agent trenutno kreće
 
     void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        navMeshAgent.SetDestination(waypoints[currentWaypoint].position);
+        navMeshAgent = GetComponent<NavMeshAgent>();  // Preuzimanje reference ka NavMeshAgent komponenti
+        navMeshAgent.SetDestination(waypoints[currentWaypoint].position);  // Kreni ka prvom waypointu
     }
 
     void Update()
     {
-        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)  // Da li je agent stigao dovoljno blizu krajnjoj tački?
         {
-            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
-            navMeshAgent.SetDestination(waypoints[currentWaypoint].position);
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;  // Odredi indeks sljedeće tačke
+            navMeshAgent.SetDestination(waypoints[currentWaypoint].position);  // Kreni ka sljedećoj tački
         }
     }
 }
