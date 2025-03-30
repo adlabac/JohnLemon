7d48872374cb267c25552c3db85ba96e71edc0f7
Aleksandar Dlabač <aleksandar@dlabac.com>
2025-03-30 11:10:17 +0200

    Ažurirani komentari u skripti GameEnding

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -12,14 +12,14 @@ public class GameEnding : MonoBehaviour
     public float fadeDuration = 1f;  // Trajanje fade efekta u sekundama
     public float displayImageDuration = 1f;  // Vrijeme prikazivanja završne slike, nakon završetka fade efekta
 
-    public AudioSource exitAudio;
-    public AudioSource caughtAudio;
+    public AudioSource exitAudio;  // Referenca ka zvuku  koji se reprodukuje kada se igra uspješno završi
+    public AudioSource caughtAudio;  // Referenca ka zvuku  koji se reprodukuje kada je john uhvaćen
 
     bool playerAtExit = false;  // Flag koji govori da li je igrač stigao do kraja nivoa
     bool playerCaught = false;  // Flag koji govori da li je igrač uhvaćen
 
     float timer = 0f;  // Tajmer za fade i kraj/restart nivoa
-    bool audioPlayed = false;
+    bool audioPlayed = false;  // Flag koji govori da li je zvuk kraja nivoa/hvatanja već reprodukovan
 
     public void CaughtPlayer()  // Javna metoda koja omogućava da se igrač proglasi uhvaćenim
     {
@@ -47,10 +47,10 @@ public class GameEnding : MonoBehaviour
 
     void EndLevel(CanvasGroup image, AudioSource audio, bool restart)
     {
-        if (!audioPlayed)
+        if (!audioPlayed)  // Da li je zvuk već reprodukovan?
         {
-            audio.Play();
-            audioPlayed = true;
+            audio.Play();  // Ako nije, reprodukuj željeni zvuk
+            audioPlayed = true;  // Postavi flag, da se u sljedećem pozivu funkcije ne bi ponovo pokrenula reprodukcija od početka
         }
 
         timer += Time.deltaTime;  // Uvećaj tajmer za vrijeme proteklo od prethodnog frejma
